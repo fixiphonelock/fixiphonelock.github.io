69d1a593da79b6f0987793d0d7da56f4953a0043
Cuong Huyen <cuonghuyen@Cuongs-MacBook-Air.local>
2016-10-23 21:07:38 +0700

    modified:   fixiphonelock.sh

--- fixiphonelock.sh
+++ fixiphonelock.sh
@@ -1,12 +1,6 @@
 #/bin/sh
 # script by huyenxone
-# fixiphonelock.github.io
 # fix full iphone lock Nhat
-
-#cd "/var/mobile/Library/Carrier Bundles/Overlay"
-#for fl in *.plist; do
-#	sed '/<key>SupportSupplementaryServices[^<]*<\/key>/,/>/s/false/true/g' "$fl" | tee "$fl"
-#done
 
 chmod -w "/var/mobile/Library/Carrier Bundles/Overlay"
 # tim xoa
@@ -18,13 +12,30 @@ chown mobile "/var/mobile/Library/Carrier Bundles/iPhone"
 rm -rf "/System/Library/Carrier Bundles/iPhone/44010"
 rm -rf "/System/Library/Carrier Bundles/iPhone/44020"
 rm -rf "/System/Library/Carrier Bundles/iPhone/44050"
-
+rm -rf "/System/Library/Carrier Bundles/iPhone/311480"
+rm -rf "/System/Library/Carrier Bundles/iPhone/20404"
+rm -rf "/System/Library/Carrier Bundles/iPhone/310120"
+rm -rf "/System/Library/Carrier Bundles/iPhone/310120-GID2_01"
+rm -rf "/System/Library/Carrier Bundles/iPhone/310260_ID-89012600"
+rm -rf "/System/Library/Carrier Bundles/iPhone/310260_ID-89012605"
+rm -rf "/System/Library/Carrier Bundles/iPhone/310410"
+ 
 rm -rf "/System/Library/Carrier Bundles/iPhone/Docomo_jp.bundle/"
 mkdir -p "/System/Library/Carrier Bundles/iPhone/Docomo_jp.bundle/"
 rm -rf "/System/Library/Carrier Bundles/iPhone/Softbank_jp.bundle/"
 mkdir -p "/System/Library/Carrier Bundles/iPhone/Softbank_jp.bundle/"
-rm -rf "/System/Library/Carrier Bundles/iPhone/KDDI_jp.bundle/"
-mkdir -p "/System/Library/Carrier Bundles/iPhone/KDDI_jp.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/KDDI _LTE_jp.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/KDDI _LTE_jp.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/Verizon_LTE_US.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/Verizon_LTE_US.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/Sprint _LTE_US.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/Sprint _LTE_US.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/Sprint_CSIM_LTE_US.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/Sprint_CSIM_LTE_US.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/Tmobile_US.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/Tmobile_US.bundle/"
+rm -rf "/System/Library/Carrier Bundles/iPhone/ATT_US.bundle/"
+mkdir -p "/System/Library/Carrier Bundles/iPhone/ATT_US.bundle/"
 
 rm -rf "/System/Library/Carrier Bundles/iPhone/Unknown.bundle/"
 cp -R "/System/Library/Carrier Bundles/iPhone/Viettel_vn.bundle/" "/System/Library/Carrier Bundles/iPhone/Unknown.bundle/"
